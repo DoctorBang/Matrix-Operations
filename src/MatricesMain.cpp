a257e2e486005b26ab287b4f256ea6462e270e14
DoctorBang <armand.rathgeb@protonmail.com>
2021-02-19 16:32:16 -0600

    Initial commit

---
+++ src/MatricesMain.cpp
@@ -0,0 +1,71 @@
+#include "MatrixFunctions.h"
+
+int main(){
+	char inputKey;
+	/*
+    *Initialized col and row1 to different variables so that 
+    *loops that ask for them to be different sizes don't break.
+    *Didn't leave them uninitilized because then they'll have 
+    *random junk that just may or may not be equal.
+    */
+	int row = 0, col = 0, row1 = 1, col1 = 1;
+	struct MatrixFunctions matrices;
+	
+	std::cout << "A) Multiply matrices. \n";
+	std::cout << "B) Add matrices. \n";
+	std::cout << "Q) Quit. \n";
+	std::cin.get(inputKey);
+
+	switch(inputKey){
+		case 'a':	//Section for multiplying matrices
+		case 'A':{
+			while(col != row1){
+				std::cout << "Please enter the size of the matrix.\n";
+				matrices.inputMatrixSize(&row, &col);
+				std::cout << "Please enter the size of the second matrix.\n";
+				matrices.inputMatrixSize(&row1, &col1);
+				if(col != row1){					
+					std::cout << "You cannot multiply these two matrices. ";
+					std::cout << "The columns of the first matrix must be equal to the ";
+					std::cout << "columns of the second matrix.\n";
+				}
+			}
+
+			double matrix1[row][col] = {}, matrix2[row1][col1] = {}, finalMatrix[row][col1] = {};
+ 
+			std::cout << "Please enter the values for each matrix:\n";
+			std::cout << "Matrix 1: \n";
+			matrices.matrixInput( (double*) matrix1, row, col);	//Cast the arrays to a double pointer in order to pass it by reference
+			std::cout << "Matrix 2: \n";
+			matrices.matrixInput( (double*) matrix2, row1, col1);
+
+			matrices.multiplyMatrices( (double*) finalMatrix, (double *)matrix1, (double *)matrix2, row, col);
+			matrices.printMatrix( (double*) finalMatrix, row, col1);
+			break;
+		}
+		case 'b':	//Section for adding matrices
+		case 'B':{
+			while(!col){
+                std::cout << "Please enter the size of the matrices: \n";
+                matrices.inputMatrixSize(&row, &col);
+            }
+			double matrix1[row][col] = {}, matrix2[row][col] = {};
+
+            std::cout << "Please input the values in each matrix:\n";
+            std::cout << "Matrix 1:\n";
+			matrices.matrixInput((double*) matrix1, row, col);
+			std::cout << "Matrix 2:\n";
+			matrices.matrixInput((double*) matrix2, row, col);
+			
+			matrices.addMatrices((double*) matrix1, (double*) matrix2, row, col);
+			matrices.printMatrix((double*) matrix1, row, col);
+			break;
+		}
+		default:
+			std::cout << "Error occured\n";
+	}
+
+	return 0;
+			
+
+}
---
+++ src/MatrixFunctions.h
@@ -0,0 +1,60 @@
+#ifndef MATRIXFUNCTIONS_H
+#define MATRIXFUNCTIONS_H
+
+#include <iostream>
+
+struct MatrixFunctions{
+    public:
+        void multiplyMatrices(double* finalMatrix, double* matrix1, double* matrix2, int row, int col){
+			/*
+			*Matrices multiplied here
+			*Good website to see how matrix multiplition is done:
+			*https://www.mathsisfun.com/algebra/matrix-multiplying.html
+			*/
+			for(int i = 0; i < row; i++){
+				for(int j = 0; j < row; j++){
+					for(int k = 0; k < col; k++){
+                        *((finalMatrix + i *row) + j) += *((matrix1 + i *col) +k) * *((matrix2 + k * row) + j);
+					}
+				}
+			}
+			
+		}	
+
+        void addMatrices(double* finalMatrix, double* matrix, int col, int row){
+            for(int i = 0; i < row; i++){
+                for(int j = 0; j < col; j++)
+                    *((finalMatrix + i *row) +j) += *((matrix +i *row) +j);
+            }
+
+        }
+
+        void inputMatrixSize(int* row, int* column){
+            std::cout << "Rows: ";
+            std::cin >> *row;
+            std::cout << "Columns: ";
+            std::cin >> *column;
+        }
+
+        void matrixInput(double* matrix, int rows, int columns){
+            for(int i = 0; i < rows; i++){
+                for(int j = 0; j < columns; j++){
+                    std::cout << "Matrix position (" << i + 1 << ")(" << j + 1 << ") ";
+                    std::cin >> *((matrix+i*columns) + j);
+                }
+            }
+            std::cout << "\n";
+        }
+
+        void printMatrix(double* matrix, int rows, int columns){
+            for(int i = 0; i < rows; i++){
+                for(int j = 0; j < columns; j++)
+                    std::cout << *((matrix + i * columns) + j) << " ";
+                std::cout << "\n";
+            }
+        }
+
+      
+
+};
+#endif
