bcad098548a47b27ccff8ba8a5006a1225b98b3a
DoctorBang <doctor.bang@impactclient.net>
2021-12-30 18:17:25 -0600

    Reformat tabs, add extra operators

--- src/MatricesMain.cpp
+++ src/MatricesMain.cpp
@@ -1,45 +1,39 @@
 #include "MatrixFunctions.h"
 
 void print_menu(){
-  std::cout << "0: Multiply\n" 
-    << "1: Add\n"
-    << "2: Subtract\n";
+    std::cout << "0: Multiply\n" 
+      << "1: Add\n"
+      << "2: Subtract\n";
+}
+
+void get_in(Matrix<double>& a, Matrix<double>& b) {
+    std::cout << "Enter matrix 1\n";
+    a.input();
+    std::cout << "Enter matrix 2\n";
+    b.input();
 }
 
 int main(){
-  Matrix<double> a,b,c;
-  print_menu();
-  int input;
-  std::cin >> input;
+    Matrix<double> a,b;
+    print_menu();
+    int input;
+    std::cin >> input;
   
-  switch(input){
-  case 0:
-    std::cout << "Enter matrix 1\n";
-    a.input();
-    std::cout << "Enter matrix 2\n";
-    b.input();
-    c = a * b;
-    std::cout << c;
-    break;
-  case 1:
-    std::cout << "Enter matrix 1\n";
-    a.input();
-    std::cout << "Enter matrix 2\n";
-    b.input();
-    c = a + b;
-    std::cout << c;
-    break;
-  case 2:
-    std::cout << "Enter matrix 1\n";
-    a.input();
-    std::cout << "Enter matrix 2\n";
-    b.input();
-    c = a - b;
-    std::cout << c;
-    break;
-  default:
-    std::cout << "Menu option incorrect\n";
-  }
-
-  return 0;
+    switch(input){
+    case 0:
+        get_in(a,b);
+        std::cout << a*b;
+        break;
+    case 1:
+        get_in(a,b);
+        std::cout << a + b;
+        break;
+    case 2:
+        get_in(a,b);  
+        std::cout << a - b;
+        break;
+    default:
+        std::cout << "Menu option incorrect\n";
+    }
+    return 0;
 }
--- src/MatrixFunctions.h
+++ src/MatrixFunctions.h
@@ -2,99 +2,124 @@
 #define MATRIXFUNCTIONS_H
 
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <initializer_list>
 
-#define MATRIX(X) std::vector<std::vector<X>>
 #define LIST(X) std::initializer_list<std::initializer_list<X>>
 
 template<typename T>
 struct Matrix {
 public:
-  MATRIX(T) m;
+    // Innter matrix
+    std::vector<std::vector<T>> m;
 
-  Matrix() = default;
+    Matrix() = default;
 
-  Matrix(LIST(T) list){
-    std::vector<T> temp;
-    for(auto i : list){
-      for(auto j : i){
-        temp.push_back(j);
-      }
-      m.push_back(temp);
-      temp.clear();
-    }
-  }
-
-  friend Matrix<T> operator*(Matrix<T> a, Matrix<T> b){
-    T sum = 0;
-    Matrix<T> c;
-    std::vector<T> temp;
-    for(size_t i = 0; i < a.m.size(); ++i) {
-      for(size_t j = 0; j < b.m[0].size(); ++j) {
-        for(size_t k = 0; k < a.m[0].size(); ++k){
-          sum += a.m[i][k] * b.m[k][j];
+    Matrix(LIST(T) list){
+        std::vector<T> temp;
+        for(auto i : list){
+          for(auto j : i){
+            temp.push_back(j);
+          }
+          m.push_back(temp);
+          temp.clear();
         }
-        temp.push_back(sum);
-        sum = 0;
-      }
-      c.m.push_back(temp);
-      temp.clear();
     }
-    return c;
-  } 
 
-  friend Matrix<T> operator+(Matrix<T> a, Matrix<T> b){
-    Matrix<T> c;
-    std::vector<T> temp;
-    for(size_t i = 0; i < a.m.size(); ++i){
-      for(size_t j = 0; j < a.m[i].size(); ++j){
-        temp.push_back(a.m[i][j] + b.m[i][j]); 
-      }
-      c.m.push_back(temp);
-      temp.clear();
+    Matrix(const Matrix<T>& a){
+        std::vector<T> temp;
+        for(auto i : a.m) {
+            for(auto j : i){
+                temp.push_back(j);
+            }
+          this->m.push_back(temp);
+          temp.clear();
+        }
     }
-    return c;
-  }
 
-  friend Matrix<T> operator-(Matrix<T> a, Matrix<T> b){
-    Matrix<T> c;
-    std::vector<T> temp;
-    for(size_t i = 0; i < a.m.size(); ++i){
-      for(size_t j = 0; j < a.m[i].size(); ++j){
-        temp.push_back(a.m[i][j] - b.m[i][j]); 
-      }
-      c.m.push_back(temp);
-      temp.clear();
-    }
-    return c;
-  }
-  friend std::ostream& operator<<(std::ostream &output, Matrix<T> &mat){
-    for(auto i : mat.m){
-	for(auto j : i){
-	    output << j << " ";	
-	}
-	output << "\n";
-    }	
-    return output;
-  }
+    friend Matrix<T> operator*(Matrix<T> a, Matrix<T> b){
+        T sum = 0;
+        Matrix<T> c;
+        std::vector<T> temp;
+        for(size_t i = 0; i < a.m.size(); ++i) {
+            for(size_t j = 0; j < b.m[0].size(); ++j) {
+                for(size_t k = 0; k < a.m[0].size(); ++k){
+                    sum += a.m[i][k] * b.m[k][j];
+                }
+            temp.push_back(sum);
+            sum = 0;
+          }
+          c.m.push_back(temp);
+          temp.clear();
+        } 
+        return c;
+    } 
 
-  void input(){
-    size_t row, col;
-    std::cout << "Enter the row size: ";
-    std::cin >> row;
-    std::cout << "Enter the column size: ";
-    std::cin >> col;
-    T val;
-    std::vector<T> temp;
-    for(size_t i = 0; i < row; ++i){
-      for(size_t j = 0; j < col; ++j){
-        std::cin >> val;
-        temp.push_back(val);
-      }
-      this->m.push_back(temp);
-      temp.clear();
+    friend Matrix<T> operator+(Matrix<T> a, Matrix<T> b){
+        Matrix<T> c;
+        std::vector<T> temp;
+        for(size_t i = 0; i < a.m.size(); ++i){
+            for(size_t j = 0; j < a.m[i].size(); ++j){
+                temp.push_back(a.m[i][j] + b.m[i][j]); 
+            }
+            c.m.push_back(temp);
+            temp.clear();
+        }
+        return c;
     }
-  }
+
+    friend Matrix<T> operator-(Matrix<T> a, Matrix<T> b){
+        Matrix<T> c;
+        std::vector<T> temp;
+        for(size_t i = 0; i < a.m.size(); ++i){
+            for(size_t j = 0; j < a.m[i].size(); ++j){
+                temp.push_back(a.m[i][j] - b.m[i][j]); 
+            }
+            c.m.push_back(temp);
+            temp.clear();
+        }
+        return c;
+    }
+
+    friend std::ostream& operator<<(std::ostream &output, Matrix<T> mat){
+        for(auto i : mat.m){
+	          for(auto j : i){
+	              output << j << "\t";
+	          }
+	          output << "\n";
+        }	
+        return output;
+    }
+
+    Matrix<T> operator=(Matrix<T> a){
+        return a;
+    }
+
+    std::vector<T> operator[](size_t i) const {
+        return this->m[i];
+    }
+
+    std::vector<T>& operator[](size_t i){
+        return this->m[i];
+    }
+
+    void input(){
+        size_t row, col;
+        std::cout << "Enter the row size: ";
+        std::cin >> row;
+        std::cout << "Enter the column size: ";
+        std::cin >> col;
+        T val;
+        std::vector<T> temp;
+        for(size_t i = 0; i < row; ++i){
+            for(size_t j = 0; j < col; ++j){
+                std::cin >> val;
+                temp.push_back(val);
+            }
+            this->m.push_back(temp);
+            temp.clear();
+        }
+    }
 };
 #endif
